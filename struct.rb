21fbb4eca73ad66c7fb1ce46e79c2e114f256c7e
hawkk9 <yuichi19930508@gmail.com>
2020-03-25 23:37:57 +0900

    Struct keyword_init

--- struct.rb
+++ struct.rb
@@ -1,8 +1,15 @@
 class StructSandbox
   class << self
-    Param = Struct.new( :atk, :def)
+    Param1 = Struct.new( :atk, :def)
+    Param2 = Struct.new( :atk, :def, keyword_init: true)
     def basic_usage
-      param = Param.new(100, 50)
+      param = Param1.new(100, 50)
+      p param
+      p "atk:#{param.atk}, def:#{param.def}"
+    end
+
+    def keyword_init
+      param = Param2.new({atk: 100, def: 50})
       p param
       p "atk:#{param.atk}, def:#{param.def}"
     end
